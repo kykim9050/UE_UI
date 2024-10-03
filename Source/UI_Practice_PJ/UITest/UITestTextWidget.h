9c62346b9484aeee1852dbf0b7c127d49f5462bf
kykim9050 <kykim9050@naver.com>
2024-10-03 20:48:44 +0900

    UITextUserWidgetBase 클래스 추가, UITestTextWidget 클래스 추가 및 블루프린트 생성

---
+++ Source/UI_Practice_PJ/UITest/UITestTextWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UITest/UITestUserWidgetBase.h"
+#include "UITestTextWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UI_PRACTICE_PJ_API UUITestTextWidget : public UUITestUserWidgetBase
+{
+	GENERATED_BODY()
+	
+};
