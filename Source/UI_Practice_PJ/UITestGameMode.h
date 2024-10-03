73ac0c321262d60feb6f8293e5107ff5814fa1c9
kykim9050 <kykim9050@naver.com>
2024-10-03 19:42:24 +0900

    UITestGameMode C++코드, 블루프린트 추가

---
+++ Source/UI_Practice_PJ/UITestGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "UITestGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UI_PRACTICE_PJ_API AUITestGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
