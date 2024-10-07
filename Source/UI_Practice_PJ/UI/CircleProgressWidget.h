ae5d30404b298c5bd2b2721bda74b087bd031b4e
kykim9050 <kykim9050@naver.com>
2024-10-07 09:01:54 +0900

    김경윤 : C++ 코드 변환을 위한 클래스 추가

---
+++ Source/UI_Practice_PJ/UI/CircleProgressWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/KKYUserWidgetBase.h"
+#include "CircleProgressWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UI_PRACTICE_PJ_API UCircleProgressWidget : public UKKYUserWidgetBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/UI_Practice_PJ/UI/KKYUserWidgetBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "KKYUserWidgetBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UI_PRACTICE_PJ_API UKKYUserWidgetBase : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
