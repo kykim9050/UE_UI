ceb62fad69eedd2af3f712ad468783d841b79ab7
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-10-07 10:00:18 +0900

    김경윤 : SizeBox 위젯 캐스팅으로 매칭

--- Source/UI_Practice_PJ/UI/CircleProgressWidget.cpp
+++ Source/UI_Practice_PJ/UI/CircleProgressWidget.cpp
@@ -1,5 +1,40 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "UI/CircleProgressWidget.h"
+#include "Components/SizeBox.h"
 
+
+UCircleProgressWidget::UCircleProgressWidget()
+{
+	
+}
+
+void UCircleProgressWidget::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	SizeBoxInit();
+
+	SetBoxSize(300.0f, 300.0f);
+}
+
+void UCircleProgressWidget::SizeBoxInit()
+{
+	SizeBox = Cast<USizeBox>(GetWidgetFromName(TEXT("CP_SizeBox")));
+
+	if (nullptr == SizeBox)
+	{
+		UE_LOG(LogType, Fatal, TEXT("if (nullptr == SizeBox)"));
+		return;
+	}
+}
+
+void UCircleProgressWidget::SetBoxSize(float _Width, float _Height)
+{
+	if (nullptr == SizeBox)
+	{
+		return;
+	}
+	
+	SizeBox->WidthOverride = _Width;
+	SizeBox->HeightOverride = _Height;
+}
