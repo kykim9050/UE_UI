caa9e1987f4765c0d69fe1c86fee6b61534252b6
kykim9050 <kykim9050@naver.com>
2024-10-08 09:40:25 +0900

    김경윤 : Image widget에 BrushResourceObject 연동

--- Source/UI_Practice_PJ/UI/KKYUserWidgetBase.h
+++ Source/UI_Practice_PJ/UI/KKYUserWidgetBase.h
@@ -17,18 +17,24 @@ class UI_PRACTICE_PJ_API UKKYUserWidgetBase : public UUserWidget
 public:
 
 protected:
-	template<typename ClassType>
-	ClassType* WidgetComponentInit(const FName& _WidgetName)
+	/// <summary>
+	/// 위젯 컴포넌트 초기화 (해당 위젯 안에 컴포넌트로 존재한다면 주소값 반환)
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="_WidgetName"></param>
+	/// <returns></returns>
+	template<typename T>
+	T* WidgetComponentInit(const FName& _WidgetName)
 	{
-		ClassType* CurWidget = Cast<ClassType>(GetWidgetFromName(_WidgetName));
+		T* WidgetComp = Cast<T>(GetWidgetFromName(_WidgetName));
 
-		if (nullptr == CurWidget)
+		if (nullptr == WidgetComp)
 		{
-			UE_LOG(LogType, Fatal, TEXT("if (nullptr == CurWidget)"));
+			UE_LOG(LogType, Fatal, TEXT("if (nullptr == WidgetComp)"));
 			return nullptr;
 		}
 
-		return CurWidget;
+		return WidgetComp;
 	}
 
 private:
