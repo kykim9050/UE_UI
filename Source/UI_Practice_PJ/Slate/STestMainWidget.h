ec38ab3d900af652b91d314afbc567e7213aa92a
kykim9050 <kykim9050@naver.com>
2024-10-19 11:38:56 +0900

    김경윤 : Slate Widget 관련 클래스, 블루프린트 추가

---
+++ Source/UI_Practice_PJ/Slate/STestMainWidget.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "SBaseWidget.h"
+
+/**
+ * 
+ */
+class STestMainWidget : public SBaseWidget
+{	
+public:
+	SLATE_BEGIN_ARGS(STestMainWidget) {}
+
+	SLATE_END_ARGS()
+
+	virtual void Construct(const FArguments& _Args);
+
+protected:
+
+private:
+};
