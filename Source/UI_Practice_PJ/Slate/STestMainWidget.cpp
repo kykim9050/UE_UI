5dc5ff8a67dd38c440b959f65209b6aadb7499df
kykim9050 <kykim9050@naver.com>
2024-10-19 12:20:03 +0900

    김경윤 : 화면에 Slate UI Text 출력

--- Source/UI_Practice_PJ/Slate/STestMainWidget.cpp
+++ Source/UI_Practice_PJ/Slate/STestMainWidget.cpp
@@ -3,9 +3,26 @@
 
 #include "Slate/STestMainWidget.h"
 
+#define LOCTEXT_NAMESPACE "MainMenu"
+
 void STestMainWidget::Construct(const FArguments& _Args)
 {
 	bCanSupportFocus = true;
 
 	OwningHUD = _Args._OwningHUD;
+
+	const FText TitleText = LOCTEXT("GameTitle", "Slate UI Challenge");
+
+	FSlateFontInfo TitleTextStyle = FCoreStyle::Get().GetFontStyle("EmbossedText");
+	TitleTextStyle.Size = 60.0f;
+
+	ChildSlot
+		[
+			SNew(STextBlock)
+				.Font(TitleTextStyle)
+				.Text(TitleText)
+				.Justification(ETextJustify::Center)
+		];
 }
+
+#undef LOCTEXT_NAMESPACE
