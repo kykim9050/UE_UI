d0083b2c6ac3edd149ba25cd298d87b15926ac14
kykim9050 <kykim9050@naver.com>
2024-10-20 10:19:38 +0900

    김경윤 : SUniformGridPanelWidget 내용 추가

--- Source/UI_Practice_PJ/Slate/SUniformGridPanelsWidget.cpp
+++ Source/UI_Practice_PJ/Slate/SUniformGridPanelsWidget.cpp
@@ -11,10 +11,57 @@ void SUniformGridPanelsWidget::Construct(const FArguments& _Args)
 
 	OwningHUD = _Args._OwningHUD;
 
-	//const FText TitleText = LOCTEXT("Title", "Slate UI Challenge");
-	//FSlateFontInfo TitleTextStyle = FCoreStyle::Get().GetFontStyle("EmbossedText");
-	//TitleTextStyle.Size = 60.0f;
+	FSlateFontInfo TitleTextStyle = FCoreStyle::Get().GetFontStyle("EmbossedText");
+	TitleTextStyle.Size = 100.0f;
 
-
-
+	ChildSlot
+	[
+		SNew(SUniformGridPanel)
+		.SlotPadding( FMargin(5.0f))
+		+SUniformGridPanel::Slot(0,0)
+		.HAlign(HAlign_Center)
+		[
+			SNew(STextBlock)
+			.Font(TitleTextStyle)
+			.Text(LOCTEXT("UGPFirst", "UGPFirst"))
+			.Justification(ETextJustify::Center)
+		]
+		+SUniformGridPanel::Slot(0,1)
+		.HAlign(HAlign_Center)
+		[
+			SNew(STextBlock)
+			.Font(TitleTextStyle)
+			.Text(LOCTEXT("UGPSecond", "UGPSecond"))
+			.Justification(ETextJustify::Center)
+		]
+		+SUniformGridPanel::Slot(0,2)
+		.HAlign(HAlign_Center)
+		[
+			SNew(STextBlock)
+			.Font(TitleTextStyle)
+			.Text(LOCTEXT("UGPThird", "UGPThird"))
+			.Justification(ETextJustify::Center)
+		]
+		+SUniformGridPanel::Slot(1,0)
+		[
+			SNew(STextBlock)
+			.Font(TitleTextStyle)
+			.Text(LOCTEXT("UGPForth", "UGPForth"))
+			.Justification(ETextJustify::Center)
+		]
+		+SUniformGridPanel::Slot(1,1)
+		[
+			SNew(STextBlock)
+			.Font(TitleTextStyle)
+			.Text(LOCTEXT("UGPFifth", "UGPFifth"))
+			.Justification(ETextJustify::Center)
+		]
+		+SUniformGridPanel::Slot(2,2)
+		[
+			SNew(STextBlock)
+			.Font(TitleTextStyle)
+			.Text(LOCTEXT("UGPSixth", "UGPSixth"))
+			.Justification(ETextJustify::Center)
+		]
+	];
 }
