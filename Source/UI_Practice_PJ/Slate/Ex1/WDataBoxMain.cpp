da530661cb3f7b5fa301bfb083bb300c4e52fc06
kykim9050 <kykim9050@naver.com>
2024-10-22 11:39:56 +0900

    김경윤 : SDataBoxMain에 검은색 화면에 Text 출력할 수 있도록 기능 구현

--- Source/UI_Practice_PJ/Slate/Ex1/WDataBoxMain.cpp
+++ Source/UI_Practice_PJ/Slate/Ex1/WDataBoxMain.cpp
@@ -6,7 +6,33 @@
 
 void SDataBoxMain::Construct(const FArguments& InArgs)
 {
+	ChildSlot
+	.HAlign(HAlign_Fill)
+	.VAlign(VAlign_Fill)
+	[
+		SNew(SOverlay)
+		+SOverlay::Slot()
+		.HAlign(HAlign_Fill)
+		.VAlign(VAlign_Fill)
+		[
+			SNew(SImage)
+			.Image(FCoreStyle::Get().GetBrush("BlackBrush"))
+		]
+		+SOverlay::Slot()
+		.HAlign(HAlign_Center)
+		.VAlign(VAlign_Center)
+		[
+			SNew(STextBlock)
+			.Text(this, &SDataBoxMain::GetDataTitle)
+			.ColorAndOpacity(FSlateColor(FLinearColor::White))
+			.Font(FSlateFontInfo(FPaths::EngineContentDir() / TEXT("Slate/Fonts/Roboto-Regular.ttf"), 24))
+		]
+	];
+}
 
+FText SDataBoxMain::GetDataTitle() const
+{
+	return FText::FromString(DataTitle);
 }
 
 void UWDataBoxMain::ReleaseSlateResources(bool bReleaseChildren)
